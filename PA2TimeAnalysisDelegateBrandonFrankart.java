3d6078c22d2bd9ca65eb47b998353d738e680ea6
hbergcbrandon <163863122+hbergcbrandon@users.noreply.github.com>
2024-10-15 03:19:06 -0400

    Update PA2TimeAnalysisDelegateBrandonFrankart.java
    updating author credits

--- PA2TimeAnalysisDelegateBrandonFrankart.java
+++ PA2TimeAnalysisDelegateBrandonFrankart.java
@@ -2,7 +2,7 @@ package pa2timeanalysisbrandonfrankart;
 
 /**
  *
- * @author jmfra and brando
+ * @author Jacob Frankart and Cole Brandon
  */
 public final class PA2TimeAnalysisDelegateBrandonFrankart
 {
